3419d99af066cf090ff2daf3b6cfb3db7f473f49
Yoga Triatmojo <yogazen137@gmail.com>
2022-11-10 11:30:56 +0800

    add gambar pln

--- editMobil.dart
+++ editMobil.dart
@@ -1,27 +1,107 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'main.dart';
+import 'package:flutter/services.dart';
 
-class EditMobil extends StatelessWidget {
+class EditMobil extends StatefulWidget {
   const EditMobil({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.greenAccent,
-        appBar: AppBar(
-          elevation: 0.0,
-          backgroundColor: Colors.green,
-          centerTitle: true,
-          title: Text('Welcome to PT PLN UIKL Monitoring Driver'),
-        ),
-        body: Center(
-            child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [],
-        )),
+  _EditMobilState createState() => _EditMobilState();
+}
+
+class _EditMobilState extends State<EditMobil> {
+
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.greenAccent,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.green,
+        centerTitle: true,
+        title: Text('Welcome to PT PLN UIKL Monitoring Driver'),
+      ),
+      body: FutureBuilder(
+        future: ReadJson(),
+        builder: (context, data){
+          if (data.hasError){
+            return Center(child: Text('${data.error}'));
+          }
+          else if(data.hasData) {
+            var dataMobil = data.data as List<DataMobil>;
+            return Column(
+              children:[
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: dataMobil == null ? 0 : dataMobil.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          elevation: 5,
+                          margin: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                          child: Container(
+                            padding: EdgeInsets.only(bottom: 8),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(left: 8),
+                                  child:
+                                  Text(
+                                    dataMobil[index].merek.toString(),
+                                    style: TextStyle(
+                                        fontSize: 15
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(left: 8),
+                                  child: Text(dataMobil[index].platmobil.toString()),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }
+                    ),
+                ),
+                ButtonIcon(
+                    buttonAction: (){},
+                    imgDirectory: 'assets/icons/addIcon.jpg',
+                    iconSize: 50,
+                )
+              ]
+            );
+          }
+          else{
+            return Center(
+              child: CircularProgressIndicator()
+            );
+          }
+        },
       ),
     );
+  }
+  Future <List<DataMobil>>ReadJson() async{
+    final data = await rootBundle.loadString('assets/mobil.json');
+    final list = json.decode(data) as List<dynamic>;
+
+    return list.map((e) => DataMobil.fromJson(e)).toList();
+  }
+}
+
+class DataMobil{
+  String? merek;
+  String? platmobil;
+
+  DataMobil({
+    this.merek,
+    this.platmobil
+  });
+
+  DataMobil.fromJson(Map<String,dynamic> json){
+    merek = json['merek'];
+    platmobil = json['platmobil'];
   }
 }
--- main.dart
+++ main.dart
@@ -15,15 +15,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.greenAccent,
-        appBar: AppBar(
-          elevation: 0.0,
-          backgroundColor: Colors.green,
-          centerTitle: true,
-          title: Text('Welcome to PT PLN UIKL Monitoring Driver'),
-        ),
-        body: AppNav(),
-      ),
+          backgroundColor: Colors.greenAccent,
+          appBar: AppBar(
+            elevation: 0.0,
+            backgroundColor: Colors.green,
+            centerTitle: true,
+            title: Text('Welcome to PT PLN UIKL Monitoring Driver'),
+          ),
+          body: Center(
+              child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.fromLTRB(
+                  20,
+                  20,
+                  20,
+                  20,
+                ),
+                child: Image(
+                    height: 100,
+                    width: 70,
+                    fit: BoxFit.fitWidth,
+                    image: AssetImage('assets/images/pln.png')),
+              ),
+              AppNav(),
+            ],
+          ))),
     );
   }
 }
@@ -40,7 +57,7 @@ Asset Template
 
 Template Button
 
-Button(text, buttonAction)
+Button(text, *buttonAction, leftPad, topPad, rightPad, bottomPad)
 
 parameter :
 text = Text pada tombol
@@ -59,42 +76,104 @@ class Button extends StatelessWidget {
   final double rightPad;
   final double bottomPad;
 
-  const Button({Key? key,
-    required this.buttonAction,
-    this.text = '',
-    this.leftPad = 5,
-    this.topPad = 5,
-    this.rightPad = 5,
-    this.bottomPad = 5
-  }) : super(key: key);
+  const Button(
+      {Key? key,
+      required this.buttonAction,
+      this.text = '',
+      this.leftPad = 5,
+      this.topPad = 5,
+      this.rightPad = 5,
+      this.bottomPad = 5})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding:EdgeInsets.fromLTRB(leftPad, topPad, rightPad, bottomPad),
+        padding: EdgeInsets.fromLTRB(leftPad, topPad, rightPad, bottomPad),
         child: Container(
-          padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
           decoration: BoxDecoration(
-              border: Border.all(color: Colors.black)
-          ),
+              border: Border.all(color: Color.fromARGB(255, 37, 39, 33))),
           child: TextButton(
             style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(
+                  Color.fromARGB(255, 212, 228, 230)),
               //        primary: ,
               foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
               overlayColor: MaterialStateProperty.resolveWith<Color?>(
-                      (Set<MaterialState> states) {
-                    if (states.contains(MaterialState.hovered))
-                      return Colors.blue.withOpacity(0.50);
-                    if (states.contains(MaterialState.focused))
-                      return Colors.blue.withOpacity(0.75);
-                    return null;
-                  }
-              ),
+                  (Set<MaterialState> states) {
+                if (states.contains(MaterialState.hovered))
+                  return Colors.blue.withOpacity(0.50);
+                if (states.contains(MaterialState.focused))
+                  return Colors.blue.withOpacity(0.75);
+                return null;
+              }),
             ),
             onPressed: buttonAction,
             child: Text(text),
           ),
-        )
-    );
+        ));
+  }
+}
+
+/*
+
+Template ButtonIcon
+
+ButtonIcon(*imgDirectory, *buttonAction, iconSize, leftPad, topPad, rightPad, bottomPad)
+
+parameter :
+imgDirectory = Direktori file image untuk icon
+iconSize = ukuran icon
+buttonAction = Kode yang berjalan saat tombol ditekan
+leftPad = padding kiri
+topPad = padding atas
+rightPad = padding kanan
+bottomPad = padding bawah
+
+ */
+
+class ButtonIcon extends StatelessWidget {
+  final void Function() buttonAction;
+  final String imgDirectory;
+  final double leftPad;
+  final double topPad;
+  final double rightPad;
+  final double bottomPad;
+  final double iconSize;
+
+  const ButtonIcon(
+      {Key? key,
+      required this.buttonAction,
+      required this.imgDirectory,
+      this.leftPad = 5,
+      this.topPad = 5,
+      this.rightPad = 5,
+      this.bottomPad = 5,
+      this.iconSize = 50})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.fromLTRB(leftPad, topPad, rightPad, bottomPad),
+        child: Container(
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          child: IconButton(
+            icon: Image.asset(imgDirectory),
+            iconSize: iconSize,
+            style: ButtonStyle(
+              //        primary: ,
+              foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+              overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                  (Set<MaterialState> states) {
+                if (states.contains(MaterialState.hovered))
+                  return Colors.blue.withOpacity(0.50);
+                if (states.contains(MaterialState.focused))
+                  return Colors.blue.withOpacity(0.75);
+                return null;
+              }),
+            ),
+            onPressed: buttonAction,
+          ),
+        ));
   }
 }
 
@@ -102,29 +181,46 @@ class AppNav extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Button(
-            text: 'Edit Driver',
-            buttonAction: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const EditDriver()));
-            }),
-        Button(
-            text: 'Edit Mobil',
-            buttonAction: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const EditMobil()));
-            }),
-        Button(
-            text: 'Tampilkan Data',
-            buttonAction: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const TampilData()));
-            }),
-      ],
-    ));
+        child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 142, 195, 220),
+              border: Border.all(
+                color: Color.fromARGB(255, 20, 23, 24),
+              ),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Center(
+                child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Button(
+                    text: 'Edit Driver',
+                    buttonAction: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const EditDriver()));
+                    }),
+                Button(
+                    text: 'Edit Mobil',
+                    buttonAction: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const EditMobil()));
+                    }),
+                Button(
+                    text: 'Tampilkan Data',
+                    buttonAction: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const TampilData()));
+                    }),
+              ],
+            ))));
   }
 }
